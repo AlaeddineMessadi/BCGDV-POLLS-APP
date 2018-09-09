1c24c0301c8aad4fdd2ba660890b34feb6d976a2
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2018-09-09 11:16:11 +0200

    Adding tests and screenshot

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # BCGDV Poll App
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+
+## Instructions:
+
+1. run `yarn install`
+2. run `yarn start`
+
+
--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Enzyme from 'enzyme';
+import Adapter, { mount } from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { MemoryRouter } from "react-router-dom";
+
+import App from "./App";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it("renders an app with 2 routes, home and modeler page", async () => {
+  const wrapper = Enzyme.mount(
+    <MemoryRouter initialEntries={ ["/"] } initialIndex={ 0 }>
+      <App />
+    </MemoryRouter>
+  );
+
+  // Home title is rendered
+  expect(wrapper.contains(<h1>BCGDV Poll App</h1>)).toEqual(true);
+  expect(wrapper.contains(<h2>Questions</h2>)).toEqual(true);
+  expect(wrapper.contains(<h2>Questions</h2>)).toEqual(true);
+
+
+  // When clicking the /create link
+  wrapper.find('[href="/modeler"]').simulate("click", { button: 0 });
+
+  // // Modeler title is rendered
+  expect(wrapper.contains(<h2>Create Question </h2>)).toEqual(true);
+  expect(wrapper.contains(<label>Question (*):</label>)).toEqual(true);
+
+  // When clicking the / link
+  wrapper.find('[href="/modeler"]').simulate("click", { button: 0 });
+
+  // When clicking the /details link
+  wrapper.find('[href="/details/10451"]').simulate("click", { button: 0 });
+
+  // // Modeler title is rendered
+  expect(wrapper.contains(<h2>Create Question </h2>)).toEqual(true);
+  expect(wrapper.contains(<label>Question (*):</label>)).toEqual(true);
 });
